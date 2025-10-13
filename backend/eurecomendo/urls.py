497abd610ecc953372807b027f521d7df2991d40
Potatoyz908 <euller2005@gmail.com>
2025-10-13 01:39:38 -0300

    feat: adiciona Django project inicial

---
+++ backend/eurecomendo/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('users.urls')),
+    path('api/', include('profiles.urls')),
+    path('api/', include('books.urls')),
+    path('api/', include('reviews.urls')),
+    path('api/', include('recommender.urls')),
+]
