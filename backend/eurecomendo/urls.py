8e3ccd47c7dad973bcad1b88ba686f904654d13d
Potatoyz908 <euller2005@gmail.com>
2025-10-19 21:34:37 -0300

    feat: Adiciona endpoints de autenticação com JWT no arquivo urls.py e cria coleção Postman para a API

--- backend/eurecomendo/urls.py
+++ backend/eurecomendo/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +13,6 @@ urlpatterns = [
     path('api/', include('reviews.urls')),
     path('api/', include('recommender.urls')),
     path('api/', include('library.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
