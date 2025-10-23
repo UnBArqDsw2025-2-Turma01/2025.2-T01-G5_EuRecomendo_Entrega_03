894a890dfa20f19faf1e7ec6dccffef61d2dd1f8
Renan Vieira <renanv.guedes7@gmail.com>
2025-10-22 23:54:57 -0300

    fix: redirect from home to lib page to show implementation

--- gof-factory/factory/factory/urls.py
+++ gof-factory/factory/factory/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/biblioteca/')),
     path('admin/', admin.site.urls),
     path('biblioteca/', include("biblioteca.urls"))
 ]
