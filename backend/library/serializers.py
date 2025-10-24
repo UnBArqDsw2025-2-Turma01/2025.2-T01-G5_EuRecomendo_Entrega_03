08fbee4b634e9b3b4ccd8deda03e057438c6b2d3
GabrielCastelo-31 <gabflubr@outlook.com>
2025-10-24 11:17:22 -0300

    corrije nome do id no serializer

--- backend/library/serializers.py
+++ backend/library/serializers.py
@@ -5,10 +5,12 @@ from .models import LibraryEntry
 class LibraryEntrySerializer(serializers.ModelSerializer):
     class Meta:
         model = LibraryEntry
-        fields = ["id", "user", "book", "status", "created_at", "updated_at"]
-        read_only_fields = ["id", "created_at", "updated_at"]
+        fields = ["id_library_entry", "user", "book",
+                  "status", "created_at", "updated_at"]
+        read_only_fields = ["id_library_entry", "created_at", "updated_at"]
 
 
 class AddToLibrarySerializer(serializers.Serializer):
     book_id = serializers.IntegerField()
-    status = serializers.ChoiceField(choices=LibraryEntry.STATUS_CHOICES, required=False, default="QUERO_LER")
+    status = serializers.ChoiceField(
+        choices=LibraryEntry.STATUS_CHOICES, required=False, default="QUERO_LER")
