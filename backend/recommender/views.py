3cb0948dcebe52c34e13a009a4498d1ddc555fe7
Tiago Antunes <tiagoantunes0905@gmail.com>
2025-10-24 02:46:40 -0300

    remoção do projeto antigo

--- backend/recommender/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-
-class RecommendView(APIView):
-    def get(self, request):
-        # placeholder response
-        return Response({'recommendations': []})
